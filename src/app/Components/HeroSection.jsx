a7ec33eb48e02f77b036108c8c03b5fd0e81f72c
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-10-12 16:24:25 +0100

    setting up navbar

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -11,8 +11,8 @@ const HeroSection = () => {
             This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph
             </p>
             <div>
-                <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-yellow-100 via-yellow-500 to-yellow-700 hover:bg-slate-200 text-black'>btn1</button>
-                <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-yellow-100 via-yellow-500 to-yellow-700 hover:bg-slate-800 text-black mt-3'>btn2</button>
+                <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-yellow-100 via-yellow-500 to-yellow-700 hover:text-slate-700 text-black'>btn1</button>
+                <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-yellow-100 via-yellow-500 to-yellow-700 hover:text-slate-700 text-black mt-3'>btn2</button>
             </div>
         </div>
         <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import HeroSection from './Components/HeroSection'
+import Navbar from './Components/Navbar'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black container">
+    <Navbar />
     <div className='container px-10 py-4 mx-auto'>
       <HeroSection />
     </div>
