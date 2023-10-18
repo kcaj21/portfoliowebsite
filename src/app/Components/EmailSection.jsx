8c429e263a42ec7c9c26ac06ca943fcde5d4721e
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-10-18 18:12:02 +0100

    updated more text

--- src/app/Components/EmailSection.jsx
+++ src/app/Components/EmailSection.jsx
@@ -30,7 +30,7 @@ const EmailSection = () => {
         <div className='z-10'>
         <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
         <p className='text-[#ADB7BE] mb-4 max-w-md'>
-        I'm currently looking for my first junior developer role. Please feel free to reach out!
+        Please feel free to reach out!
         </p>
         <div className='socials flex flex-row gap-2 my-2'>
             <Link target="blank" rel="noopener noreferrer" href='https://github.com/kcaj21'>
