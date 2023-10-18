64a037bd259553d3b35aa667bf22a52fd25af684
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-10-18 19:34:23 +0100

    reduced img quality in about section

--- src/app/Components/AboutSection.jsx
+++ src/app/Components/AboutSection.jsx
@@ -79,7 +79,7 @@ const AboutSection = () => {
   return (
     <section className='text-white' id='/'>
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-        <Image className='border-2 border-solid border-[#ADB7BE]' src='/images/profile.jpeg' quality={75} priority={true} width={500} height={500} alt='profilepic' />
+        <Image className='border-2 border-solid border-[#ADB7BE]' src='/images/profile.jpeg' quality={50} priority={true} width={500} height={500} alt='profilepic' />
         <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
             <h2 className= 'text-4xl font-bold text-white mb-4'>About Me</h2>
             <p className='text-base md:text-lg'>I am a full stack software developer educated at Codeclan, with a background in clinical data management. I'm currently looking for my first junior developer role and also any interesting projects I can collaborate on.</p>
