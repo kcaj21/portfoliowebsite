5cffc618ce4ec6bff79a000dd2466415c33aeb62
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-10-18 18:22:56 +0100

    updated margins on mobile

--- src/app/Components/EmailSection.jsx
+++ src/app/Components/EmailSection.jsx
@@ -26,7 +26,7 @@ const EmailSection = () => {
 
     return (
 
-    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4' id='contact'>
+    <section className='grid md:grid-cols-2 my-12 md:my-12 md:py-24 py-8 gap-4' id='contact'>
         <div className='z-10'>
         <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
         <p className='text-[#ADB7BE] mb-4 max-w-md'>
