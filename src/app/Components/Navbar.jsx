8c31374bbc1fe5e605ff36ace017eb5e6b6c48ef
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-10-19 23:48:01 +0100

    attempting click outside to close navbar

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -22,14 +22,14 @@ const navLinks = [
 const Navbar = () => {
 
     const [navbarOpen, setNavbarOpen] = useState(false);
-
+    
   return (
     <nav className='fixed top-0 left-0 right-0 z-20 bg-[#121212] bg-opacity-95 border border-t-[#33353F] border-l-transparent border-r-transparent border-b-[#33353F]'>
         <div className='flex items-center md:justify-center sm: justify-end mx-auto px-4 py-2'>
 
         <div className='mobile-menu block md:hidden'>
 
-        {
+        { 
             !navbarOpen ? (
                 <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
                 <Bars3Icon className='h-5 w-5'/>
