77f670fbdd5c60fe3be2df4fefbf5014329b2de1
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2024-01-05 21:40:06 +0000

    fixed tabButton scaling after adding devops section

--- src/app/Components/AboutSection.jsx
+++ src/app/Components/AboutSection.jsx
@@ -92,7 +92,7 @@ const AboutSection = () => {
 
   return (
     <section className='text-white' id='/'>
-        <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+        <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 xl:gap-16 sm:py-16 xl:px-16'>
         <Image className='border-2 border-solid border-[#ADB7BE]' src='/images/profile.jpeg' quality={25}  width={500} height={500} alt='profilepic' priority />
         <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
             <h2 className= 'text-4xl font-bold text-white mb-4'>About Me</h2>
--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
     <Navbar />
-    <div className='container mt-24 px-12 py-4 mx-auto'>
+    <div className='container mt-24 sm:px-12 px-10 py-4 mx-auto'>
       <HeroSection />
       <AboutSection />
       <ProjectSection />
